a3ffca9b57a6d2688f49210aa15d798e393ae401
Tomaž <cofek0@gmail.com>
2019-06-06 11:23:22 +0200

    fix test

--- djnd_supporters/views.py
+++ djnd_supporters/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import TemplateView
-from djnd_supporters import models, payment
+from djnd_supporters import models
+from djndonacije import payment
 
 class TestPaymentView(TemplateView):
     template_name = "test_payment.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(TestPaymentView, self).get_context_data(*args, **kwargs)
-        supporter = models.Supporter.objects.get(id=1)
-        context['token'] = payment.client_token(supporter)
+        supporter = models.Gift.objects.get(id=6)
+        context['token'] = payment.client_token(supporter.sender)
         return context
