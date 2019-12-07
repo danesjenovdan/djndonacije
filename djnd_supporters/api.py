1f5ab2fe3ba5b066eb45a8a6ea69c56bb4b86553
Tomaz <tomazkunst@gmail.com>
2019-12-07 23:47:01 +0100

    fixed get list of image: hide empty images

--- djnd_supporters/api.py
+++ djnd_supporters/api.py
@@ -303,3 +303,14 @@ class ImageViewSet(mixins.RetrieveModelMixin,
     lookup_field = 'token'
     queryset = models.Image.objects.all()
     serializer_class = serializers.ImageSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset()).exclude(image='')
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
