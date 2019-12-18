a53e22eb29b40bdb39b18780a7550de669caf8c6
Tomaz <tomazkunst@gmail.com>
2019-12-18 10:54:24 +0100

    added address to donation

--- djnd_supporters/admin.py
+++ djnd_supporters/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from djnd_supporters.models import Gift, Donation
 
-admin.site.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    readonly_fields = ('address',)
+
+    def address(self, obj):
+        if obj.subscriber:
+            return obj.subscriber.address
+        else:
+            return ''
+
+admin.site.register(Donation, DonationAdmin)
 admin.site.register(Gift)
