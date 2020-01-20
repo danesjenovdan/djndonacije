e2720fb863d80ca1f9a31bfe6d1397468aa40bde
Tomaz Kunst <tomazkunst@gmail.com>
2020-01-20 16:13:15 +0000

    added list display for donations

--- djnd_supporters/admin.py
+++ djnd_supporters/admin.py
@@ -3,6 +3,14 @@ from djnd_supporters.models import Gift, Donation
 
 class DonationAdmin(admin.ModelAdmin):
     readonly_fields = ('address',)
+    list_display = ('amount', 'subscriberName', 'address',)
+    list_filter = ('amount',)
+
+    def subscriberName(self, obj):
+        if obj.subscriber:
+            return obj.subscriber.name
+        else:
+            return ''
 
     def address(self, obj):
         if obj.subscriber:
