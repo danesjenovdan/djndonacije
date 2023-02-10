bc1ca617c3d770034a16ec9e21efa4c7dc93b574
Tomaž kunst <cofek0@gmail.com>
2023-02-10 11:19:30 +0100

    move static files from s3 to self hosting

--- djndonacije/settings/k8s.py
+++ djndonacije/settings/k8s.py
@@ -196,16 +196,16 @@ AGRUM_TOKEN = env['AGRUM_TOKEN']
 # DJANGO STORAGE SETTINGS
 if os.getenv('DJANGO_ENABLE_S3', False):
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
     AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID', '')
     AWS_SECRET_ACCESS_KEY = os.getenv('DJANGO_AWS_SECRET_ACCESS_KEY', '')
     AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME', '')
     AWS_DEFAULT_ACL = 'public-read' # if files are not public they won't show up for end users
     AWS_QUERYSTRING_AUTH = False # query strings expire and don't play nice with the cache
-    AWS_LOCATION = os.getenv('DJANGO_AWS_LOCATION', 'parladata')
+    AWS_LOCATION = os.getenv('DJANGO_AWS_LOCATION', 'podpri')
     AWS_S3_REGION_NAME = os.getenv('DJANGO_AWS_REGION_NAME', 'fr-par')
     AWS_S3_ENDPOINT_URL = os.getenv('DJANGO_AWS_S3_ENDPOINT_URL', 'https://s3.fr-par.scw.cloud')
     AWS_S3_SIGNATURE_VERSION = os.getenv('DJANGO_AWS_S3_SIGNATURE_VERSION', 's3v4')
+    AWS_S3_FILE_OVERWRITE = False
 
 
 GATEWAY = braintree.BraintreeGateway(
