a81568e6810855f5dfa0f9869914ef4e3305e06e
Patricija Brečko <patricija@danesjenovdan.si>
2024-09-18 16:21:09 +0200

    fix typo

--- frontend/src/components/Payment/Card.vue
+++ frontend/src/components/Payment/Card.vue
@@ -104,11 +104,11 @@ export default {
           fields: {
             number: {
               selector: '#cc-number',
-              placeholder: $t('paymentView.cardNumber'),
+              placeholder: this.$t('paymentView.cardNumber'),
             },
             expirationDate: {
               selector: '#cc-expirationDate',
-              placeholder: $t('paymentView.expiryDate'),
+              placeholder: this.$t('paymentView.expiryDate'),
             },
             cvv: {
               selector: '#cc-cvv',
