bef354a5e78d5b15db21272b4e1140d57dab8797
Tomaž Kunst <tomazkunst@gmail.com>
2025-10-07 15:50:33 +0200

    don't try send slack msg if ther is no slack_key

--- djndonacije/slack_utils.py
+++ djndonacije/slack_utils.py
@@ -1,12 +1,14 @@
 import slack
 from django.conf import settings
 
-sc = slack.WebClient(settings.SLACK_KEY, timeout=30)
+if settings.SLACK_KEY:
+    sc = slack.WebClient(settings.SLACK_KEY, timeout=30)
 
 
 def send_slack_msg(message, channel):
-    try:
-        sc.api_call("chat.postMessage", json={"channel": channel, "text": message})
-    except Exception as e:
-        # TODO send sentry error
-        print(e)
+    if settings.SLACK_KEY:
+        try:
+            sc.api_call("chat.postMessage", json={"channel": channel, "text": message})
+        except Exception as e:
+            # TODO send sentry error
+            print(e)
