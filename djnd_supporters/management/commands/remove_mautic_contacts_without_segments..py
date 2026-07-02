6ed76b27f5748800f2b15725d98d95c9bcbcd7f6
Tomaž <cofek0@gmail.com>
2026-07-02 13:10:34 +0200

    dry run delete mautic contacts

--- djnd_supporters/management/commands/remove_mautic_contacts_without_segments..py
+++ djnd_supporters/management/commands/remove_mautic_contacts_without_segments..py
@@ -9,19 +9,45 @@ mautic_api = MauticApi()
 
 class Command(BaseCommand):
     help = "Remove Mautic contacts without segments"
+    def add_arguments(self, parser):
+        # add dry run argument
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="Dry run, do not delete contacts",
+        )
 
     def handle(self, *args, **options):
+        dry_run = options["dry_run"]
         # segment_id = int(settings.MAUTIC_SEGMENT_ID_USERS_WITHOUT_SEGMENT)
         segment_id = int(41)
         data, status = mautic_api.getContacts(segment_alias="brez-segmenta")
 
-        for contact in data["contacts"].values():
+        for contact in list(data["contacts"].values())[:100]:
             contact_id = contact["id"]
             user_segments, status = mautic_api.getSegmentsOfContact(
                 contact_id=contact_id
             )
+            subscriber = Subscriber.objects.filter(mautic_id=contact_id).first()
+            if subscriber and subscriber.subscriptions.filter(is_active=True).exists():
+                print(
+                    f"Subscriber {contact_id} has active subscriptions, skipping."
+                )
+                continue
             if (
                 len(user_segments["lists"]) == 1
                 and int(list(user_segments["lists"].keys())[0]) == segment_id
             ):
-                mautic_api.deleteContact(contact["id"])
+                if not dry_run:
+                    print(f"Deleting contact {contact_id}...")
+                    mautic_api.deleteContact(contact_id)
+                else:
+                    print(f"Dry run: would delete contact {contact_id}...")
+                print(
+                    f"https://mautic.djnd.si/s/contacts/view/{contact_id}"
+                )
+                
+            else:
+                print(
+                    f"Subscriber {contact_id} has segments, skipping."
+                )
